adb74a82e6da03b7ece590c7d02629b8ab9e7017
Csabi1124 <horvathc138@gmail.com>
2023-04-05 00:36:59 +0200

    regisztracio frontend validalas kesz!

--- laravel_weboldal/resources/js/pages/RegisztracioPage.vue
+++ laravel_weboldal/resources/js/pages/RegisztracioPage.vue
@@ -1,98 +1,37 @@
 <template>
-
-    
     <div class="box">
-        <div class="container bg-primary bg-opacity-25">
-            <div class="row">
-                <div class="col-12 mx-auto mb-3 mt-3">
-                   
-                   <div>
-    <input name="fieldName" v-model="value" />
-    <span v-if="errorMessage && meta.touched">
-      {{ errorMessage }}
-    </span>
-  </div>
-
-           <!-- <form @submit="sendData">
-                <label for="info" class="mb-4" ><h1>Adja meg az adatokat!</h1></label><br>
-
-            <label for="username"><h2>Felhasználónév</h2></label>
-            <div class="col-6 mx-auto">
-                <input type="text" name="username" id="username" class="form-control">
-            </div>
-
-            <label for="email"><h2>E-mail</h2></label>
-            <div class="col-6 mx-auto">
-                <input type="text" name="email" id="email" class="form-control">
-            </div>
-            
-
-            <label for="password"><h2>Jelszó</h2></label>
-            <div class="col-6 mx-auto">
-               <input type="password" name="password" id="password" class="form-control "> 
-            </div>
-            <label for="password"><h2>Jelszó megerősítés</h2></label>
-            <div class="col-6 mx-auto">
-               <input type="password" name="password2" id="password2" class="form-control "> 
-            </div>
-            
-
-            <button type="submit" class="btn btn-primary mt-3">Regisztráció</button>
-<ErrorComponent v-if="error" :error="error" /> {{error}}
-
-        </form> -->
-
-        
-                </div>
-            </div>
-        </div>
-       
-
-    </div>
-</template>
-
-<script>
-import { object, string } from 'yup';
-import { useField } from 'vee-validate';
-
-const { errorMessage, meta, value } = useField('fieldName');
-export default{
-    data(){
-        const schema = yup.object({
-      email: yup.string().required().email(),
-      password: yup.string().required().min(8)});
-
-       return{
-        error:null,
-        schema,
-        
-       }
-    },
+        <div class="container bg-primary bg-opacity-25">              
+            <div class="row"> 
+<div class="col-12 mb-3 mt-3">
+    <Form @submit="submit" :validation-schema="schema">
+    <h2>Felhasználónév:</h2>  <br> <Field name="text" type="text" class="form-control mb-3" style="justify-content: center;"/>
+      <ErrorMessage name="text" class="alert alert-danger" role="alert"/>
+      
+    <h2 class="mx-auto mb-3">Email:</h2>  <br><Field name="email" class="form-control mb-3" style="justify-content: center;"/> 
+      <ErrorMessage name="email" class="alert alert-danger" role="alert"/>
+    <h2>Jelszó:</h2>  <br> <Field name="password" type="password" class="form-control mb-3" style="justify-content: center;"/>
+      <ErrorMessage name="password" class="alert alert-danger" role="alert"/>
+    <h2>Jelszó megerősítése:</h2>  <br> <Field name="password2" type="password" class="form-control mb-3" style="justify-content: center;"/>
+      <ErrorMessage name="password2" class="alert alert-danger" role="alert"/>
+      <button class="btn btn-primary mx-auto mt-3">Submit</button>
     
-   /* methods:{
-        sendData(values){
-        try{
-            const resp = axios.post("http://localhost:8881/api/user", values)
-        }catch(e){
-            this.error = e
-        }
-        }
-    },*/
-  setup() {
-    // Non-reactive because it was not explicitly defined with `reactive` or `ref`
-    const schema = yup.object({
-      email: yup.string().required().email(),
-      password: yup.string().required().min(8),
-    });
-
-    return {
-      schema,
-    };
-  },
-
-   
-}
-</script>
+    </Form>
+    </div>        
+        </div>
+        </div>
+        </div>
+  </template>
+  
+  <script setup>
+  import { Form, Field, ErrorMessage } from 'vee-validate';
+  import * as yup from 'yup';
+  const schema = yup.object({
+    email: yup.string().required("kötelező kitölteni!").email("valós E-mail címnek kell lennie!"),
+    password: yup.string().required("kötelező kitölteni!").min(8, "minimum 8 karakternek kell lennie!"),
+    password2: yup.string().oneOf([yup.ref('password'), null], 'A jelszavaknak meg kell egyezniük!'),
+    text: yup.string().required("kötelező kitölteni!").min(2, "minimum 2 karakternek kell lennie!"),
+  });
+  </script>
 
 <style scoped>
     .row{
@@ -105,5 +44,12 @@ export default{
         justify-content: center;
         
     }
-    
+    span {
+  display: block;
+  margin: 10px 0;
+}
+
+button, input {
+  display: block;
+}
 </style>
