9aaad128d0c4f7db25ada439fec2eea7e7c20f45
dbalazs95 <dbalazs9511@gmail.com>
2023-04-04 20:31:51 +0200

    advertpage elkezdve

--- laravel_weboldal/resources/js/pages/AdvertPage.vue
+++ laravel_weboldal/resources/js/pages/AdvertPage.vue
@@ -2,10 +2,20 @@
     <div class="box">
         <h1>Egy adott hirdetés</h1>
         {{$route.params.id}}
+        {{kisallat.name}}
     </div>
 </template>
 
 
 <script>
 
+export default{
+    name: "AdvertPage",
+    props:{
+        kisallat:Object
+    },
+    mounted(){
+        console.log(kisallat)
+    }
+}
 </script>
