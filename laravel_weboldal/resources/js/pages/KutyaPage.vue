17678e1e3948da83e083f3c208713f972669747f
Csabi1124 <horvathc138@gmail.com>
2023-04-05 02:47:32 +0200

    cica-kutya container kesz

--- laravel_weboldal/resources/js/pages/KutyaPage.vue
+++ laravel_weboldal/resources/js/pages/KutyaPage.vue
@@ -1,11 +1,19 @@
 <template>
     <div class="box">
         <h1>Kutya!</h1>
-        <div class="card-group">
-            <div class="row row-cols-3 row-cols-md-3"> 
+       
+        <div class="container containers">
+            <div class="row">
+                <div class="col-12 mt-3 mb-3">
+                    <div class="card-group">
+            <div class="row row-cols-4 row-cols-md-6 mx-auto" style="gap: 20px; justify-content: center;"> 
                 <advert-card v-for="pet in dogs" :key="pet.id" :id="pet.id" :pet="pet"/>
             </div>
         </div>
+                </div>
+            </div>
+        </div>
+        
     </div>
 </template>
 
@@ -33,3 +41,12 @@ export default{
     }
 }
 </script>
+<style scoped>
+.containers{
+    background-color: #3E1600;
+    border-radius: 50px;
+}
+
+    
+
+</style>
--- laravel_weboldal/resources/js/pages/MacskaPage.vue
+++ laravel_weboldal/resources/js/pages/MacskaPage.vue
@@ -1,9 +1,16 @@
 <template>
     <div class="box">
         <h1>Macska!</h1>
+
+        <div class="container containers">
+            <div class="row">
+                <div class="col-12 mt-3 mb-3">
         <div class="card-group">
-            <div class="row row-cols-3 row-cols-md-3"> 
+            <div class="row row-cols-4 row-cols-md-6 mx-auto" style="gap: 20px; justify-content: center;"> 
                 <advert-card v-for="pet in cats" :key="pet.id" :id="pet.id" :pet="pet"/>
+            </div>
+            </div>
+            </div>
             </div>
         </div>
     </div>
@@ -33,3 +40,9 @@ export default{
     }
 }
 </script>
+<style scoped>
+.containers{
+    background-color: #3E1600;
+    border-radius: 50px;
+}
+</style>
