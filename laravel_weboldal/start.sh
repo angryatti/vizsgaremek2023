6ddff016e6077537dcbec98b1f8897688c2e34f4
angryatti <angryatti@gmail.com>
2023-04-06 21:26:42 +0200

    hidden tabs dog saveall

--- laravel_weboldal/start.sh
+++ laravel_weboldal/start.sh
@@ -24,7 +24,7 @@ $COMPOSE -f docker-compose.yml -f docker-compose.$MODE.yml  up -d
 $COMPOSE exec app composer install
 $COMPOSE exec app php artisan key:generate
 $COMPOSE exec app npm install
-$COMPOSE exec app php artisan migrate:refresh --seed
+$COMPOSE exec app php artisan migrate:refresh
 $COMPOSE exec app php artisan db:seed
 npm install vite
 npm run build
