b7cae477c606bb3f24ca42c730069a1481bb93fd
angryatti <angryatti@gmail.com>
2023-04-24 21:37:34 +0200

    adverts db implemented plus born in pet_table

---
+++ laravel_weboldal/database/migrations/2023_04_24_190646_create_adverts_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('adverts', function (Blueprint $table) {
+            $table->id();
+            $table->integer('user_id');
+            $table->integer('pet_id');
+            $table->integer('settlement_id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('adverts');
+    }
+};
