17cb97bf4b383f5ccc576c7e4a9e4a329376bc62
Csabi1124 <horvathc138@gmail.com>
2023-04-29 23:31:38 +0200

    profile page kinezet alapok

--- laravel_weboldal/resources/js/pages/ProfilePage.vue
+++ laravel_weboldal/resources/js/pages/ProfilePage.vue
@@ -1,6 +1,65 @@
 <template>
 
-     <div class="box">{{ user }}</div>
+     <div class="box">
+            <h1 v-if="logged===null">Jelentlezz be!!</h1>
+
+            <div v-else>
+                
+                <div class=" sidebar d-flex flex-column flex-shrink-0 " style="width: 25%; ;
+    color: white; ">
+    
+      
+      <span class="fs-2 text-center">Profilom</span>
+    
+    <hr>
+    <ul class="nav  flex-column mb-auto">
+      <li class="nav-item">
+        <a href="#" class="nav-link fs-3" aria-current="page">
+          
+          Adataim
+        </a>
+      </li>
+      <li>
+        <a href="#" class="nav-link fs-3">
+          
+          Feltöltéseim
+        </a>
+      </li>
+      <li>
+        <a href="#" class="nav-link fs-3">
+          
+          Új állat feltöltése
+        </a>
+      </li>
+      
+    </ul>
+
+    
+  </div>
+
+  <div class="content" style="margin-left:30%">
+
+<div >
+  <h1>Az Adataim</h1>
+</div>
+
+
+
+<div>
+<h2>Adatok</h2>
+<h3>Az adataimról később</h3>
+<h3>most még nem tartunk ott</h3>
+</div>
+
+</div>
+</div>
+
+<div>
+    
+</div>
+     </div>
+
+
 </template>
 
 
@@ -30,3 +89,43 @@ export default{
     }
 }
 </script>
+
+<style scoped>
+
+a{
+    color: white;
+}
+
+
+li:hover{
+    background-color: #F5EDD8;
+    border-radius: 10px;
+    transform: scale(1.1);
+    
+}
+.nav-link:hover{
+   color:#3E1600; 
+}
+span{
+    text-decoration: underline;
+}
+
+.sidebar {
+    position: absolute;
+    padding: 20px;
+    height: 50%;
+    border-radius: 50px;
+    background-color: #3E1600;
+}
+.content {
+    position: absolute;
+    background-color: #F5EDD8;
+    width: 60%;
+    height: 50%; 
+    padding: 10px;
+    border-color: black;
+    border-style: solid;
+    border-radius: 50px;
+    color: #3E1600;
+}
+</style>
