b51a10504779000520762c7c3f5ecc51a373f9ac
Csabi1124 <horvathc138@gmail.com>
2023-05-04 21:50:01 +0200

    new pet 2

--- laravel_weboldal/resources/js/components/layout/ProfileNewPet.vue
+++ laravel_weboldal/resources/js/components/layout/ProfileNewPet.vue
@@ -3,30 +3,30 @@
                 <h1 class="text-center text-decoration-underline">Új állat feltöltése</h1>
         
             <div>
-                <Form @submit="submitForm" >
+                <Form @submit="submitForm">
 
         <h2>Állat neve:</h2>  
         <br> 
-        <Field name="user_name" v-model="user_name" type="text" class="form-control mb-3" style="justify-content: center;"/>
+        <Field name="pet_name" v-model="pet_name" type="text" class="form-control mb-3" style="justify-content: center;"/>
 
         <h2>Született:</h2>  
         <br> 
-        <Field name="user_name" v-model="user_name" type="text" class="form-control mb-3" style="justify-content: center;"/>
+        <Field name="born_name" v-model="born_name" type="text" class="form-control mb-3" style="justify-content: center;"/>
 
         <h2>Állat:</h2>  
         <br> 
-        <Field name="field" as="select">
-            <option v-for="pet in pets" :key="pet" >{{ pet }}</option>
+        <Field name="pet" as="select">
+            <option v-for="pet in pets" :key="pet" :value="pet">{{ pet }}</option>
         </Field>
 
         <h2>Fajta:</h2> 
         <br> 
-        <Field name="user_name" v-model="user_name" type="text" class="form-control mb-3" style="justify-content: center;"/>
+        <Field name="breed_name" v-model="breed_name" type="text" class="form-control mb-3" style="justify-content: center;"/>
 
         <h2>Neme:</h2>  
         <br> 
-        <Field name="field" as="select">
-            <option v-for="gender in genders" :key="gender">{{ gender }}</option>
+        <Field name="gender" as="select">
+            <option v-for="gender in genders" :key="gender" :value="gender">{{ gender }}</option>
         </Field>
 
         <h2>Kasztált:
@@ -43,18 +43,25 @@
 
         <h2>Leírás:</h2>  
         <br> 
-        <Field name="user_name" v-model="user_name" type="text" class="form-control mb-3" style="justify-content: center;"/>
+        <Field name="desc_name" v-model="desc_name" type="text" class="form-control mb-3" style="justify-content: center;"/>
 
         <h2>Megye:</h2>  
         <br> 
-        <Field name="field" as="select">
-                <option selected v-for="settlement in states" :key="settlement.id">{{ settlement.name }}</option>
+        <Field name="state" as="select">
+                <option selected v-for="settlement in states" :key="settlement.id" :value="settlement.name">{{ settlement.name }}</option>
         </Field>
       
 
-        <h2>Kontakt Infó:</h2>  
+        <h2 data-bs-toggle="tooltip" data-bs-placement="top" title="segítség: Mikor keressenek,
+               pontosabb cím meghatározás">Kontakt Infó:</h2>  
         <br> 
-        <Field name="user_name" v-model="user_name" type="text" class="form-control mb-3" style="justify-content: center;"/>
+        <Field name="contact_name" v-model="contact_name" type="text" class="form-control mb-3 pb-5"  style="justify-content: center;"/>
+
+        <button type="submit" class="btn btn-custom d-block w-100 p-3 mx-auto mt-3 fs-4">Feltöltés</button>
+
+        <button type="button" >
+  Tooltip on top
+</button>
         </Form> 
             </div>
     </div>
@@ -96,7 +103,7 @@ export default{
             this.states = response2.data
             
         }
-            
+        
         },
         components:{
             Form,
@@ -119,5 +126,15 @@ export default{
 img{
     width: 8%;
     height: auto;
+}
+button{
+    background-color: #B85B2F;
+    color: white;
+    font-weight: bolder;
+}
+button:hover{
+    background-color: #198754;
+    color: white;
+    
 }
 </style>
