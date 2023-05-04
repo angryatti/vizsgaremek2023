2863132eec08438eaa397764791846ba80249af1
dbalazs95 <dbalazs9511@gmail.com>
2023-05-04 22:33:08 +0200

    newpet javítgatások

--- laravel_weboldal/resources/js/components/layout/ProfileNewPet.vue
+++ laravel_weboldal/resources/js/components/layout/ProfileNewPet.vue
@@ -16,7 +16,7 @@
         <h2>Állat:</h2>  
         <br> 
         <Field name="species" v-model="species" as="select">
-            <option v-for="pet in pets" :key="pet" >{{ pet }}</option>
+            <option v-for="pet in pets" :value="pet" :key="pet" >{{ pet }}</option>
         </Field>
 
         <h2>Fajta:</h2> 
@@ -26,17 +26,17 @@
         <h2>Neme:</h2>  
         <br> 
         <Field name="gender" v-model="gender" as="select">
-            <option v-for="gender in genders" :key="gender">{{ gender }}</option>
+            <option v-for="gender in genders" :value="gender" :key="gender">{{ gender }}</option>
         </Field>
 
         <h2>Kasztált:
         
-        <Field name="castrated" v-model="castrated" type="checkbox" /></h2> 
+        <Field name="castrated" value="1" v-model="castrated" type="checkbox" /></h2> 
 
         <h2>kép feltöltése:</h2>  
         <div>
       <img src="pet_imgs/navbar.png" class="uploading-image" />
-      <input type="file" accept="image/jpeg" @change=uploadImage>
+      <input v-on:change="img"  type="file" accept="image/jpeg" @change=uploadImage>
    </div>
         
         
@@ -48,7 +48,7 @@
         <h2>Megye:</h2>  
         <br> 
         <Field name="state_id" v-model="state_id" as="select">
-                <option selected v-for="settlement in states" :key="settlement.id">{{ settlement.name }}</option>
+                <option selected v-for="settlement in states" :value="settlement.id" :key="settlement.id">{{ settlement.name }}</option>
         </Field>
       
 
@@ -59,9 +59,6 @@
 
         <button type="submit" class="btn btn-custom d-block w-100 p-3 mx-auto mt-3 fs-4">Feltöltés</button>
 
-        <button type="button" >
-  Tooltip on top
-</button>
         </Form> 
             </div>
     </div>
@@ -90,7 +87,7 @@ export default{
                 species: '',
                 breed: '',
                 gender: '',
-                castrated: '',
+                castrated: '0',
                 img: '',
                 description: '',
                 state_id: '',
@@ -105,8 +102,9 @@ export default{
                 reader.onload = e =>{
                     this.previewImage = e.target.result;
                     console.log(this.previewImage);
+
                 };
-                
+                return image.name
             },
                 async getStates(){
                 const response2 = await axios.get(`${import.meta.env.VITE_LARAVEL_URL}/api/states`)
@@ -115,7 +113,7 @@ export default{
             async submitForm(){
                 const user_id = localStorage.getItem('user_id')
                 try{
-                        const response = await axios.post(`${import.meta.env.VITE_LARAVEL_URL}/user/newpet`,{
+                        const response = await axios.post(`${import.meta.env.VITE_LARAVEL_URL}/api/user/newpet`,{
                         user_id: user_id,
                         pet_name: this.pet_name,
                         born: this.born,
@@ -123,7 +121,7 @@ export default{
                         species:this.species,
                         gender:this.gender,
                         castrated:this.castrated,
-                        img:this.img,
+                        img: this.img,
                         description: this.description,
                         state_id:this.state_id,
                         contact_info:this.contact_info,
@@ -132,7 +130,7 @@ export default{
                 }
                 
                 catch(error){
-                console.alert(error)
+                console.warn(error)
                 }
             }
         },
